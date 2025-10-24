8f7d624396ebca0bd782abddbc10e5303b6532f9
Sophia Ibarrola <sophiaibarrola@crc-dot1x-nat-10-239-177-221.bu.edu>
2025-10-23 20:54:45 -0400

    bug fix

--- src/components/HeaderLeft.jsx
+++ src/components/HeaderLeft.jsx
@@ -7,10 +7,10 @@ const HeaderLeft = () => {
        
         <div className = 'min-h-screen top-0 hidden md:sticky md:flex items-center justify-center bg-teal-600 text-white'>
             <ul className= 'flex flex-col gap-10'>
-                {['home', 'experience', 'project', 'contact'].map((item) => (
+                {['home', 'experience', 'projects', 'contact'].map((item) => (
                     <li key = {item} className = 'flex items-center justify-start cursor-pointer font-medium transition-all duration-200 group sm:text-lg md:text-xl xl:text-3xl'> 
                         <BiRightArrowAlt className= 'text-4xl-translate-x-5 opacity-0 transform transition-all duration-200 group-hover:opacity-100 group-hover:translate-x-0'/>
-                        <a href={'#${item}'} className=' transition-all duration-200 hover:translate-x-3'>{item}</a>
+                        <a href={`#${item}`} className=' transition-all duration-200 hover:translate-x-3'>{item}</a>
                         
                     </li>
                 ))}
